f4782dc4465c897c3335fda347547c04967aced9
EncryptedPedro <pedroakhaiz@gmail.com>
2024-11-29 18:59:53 +0000

    portfolio project complete

--- portfolio/src/components/ContactMeSection.js
+++ portfolio/src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -12,19 +12,56 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import { useAlertContext } from "../context/alertContext";
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
-  const { onOpen } = useAlertContext();
+  const { isLoading, response, submit } = useSubmit();
+  const { onOpen, onClose } = useAlertContext();
+
+  // Listen for changes to the response and trigger the alert
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+
+      // Close the alert after 3 seconds
+      setTimeout(() => {
+        onClose();
+      }, 3000);
+    }
+  }, [response, onOpen, onClose]);
 
   const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
+    initialValues: {
+      firstName: "",
+      email: "",
+      type: "hireMe",
+      comment: "",
+    },
+    onSubmit: async (values, { resetForm }) => {
+      // Call submit and store the result
+      const result = await submit("/api/submit", values);
+
+      // Display an alert based on the result
+      if (result) {
+        onOpen(result.type, result.message);
+      }
+
+      // Reset form if submission was successful
+      if (result?.type === "success") {
+        resetForm();
+      }
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      type: Yup.string()
+        .oneOf(["hireMe", "openSource", "other"], "Please select a valid type of enquiry")
+        .required("Type is required"),
+      comment: Yup.string().required("Required"),
+    }),
   });
 
   return (
@@ -39,45 +76,64 @@ const LandingSection = () => {
           Contact me
         </Heading>
         <Box p={6} rounded="md" w="100%">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <VStack spacing={4}>
-              <FormControl isInvalid={false}>
+              <FormControl isInvalid={formik.touched.firstName && formik.errors.firstName}>
                 <FormLabel htmlFor="firstName">Name</FormLabel>
                 <Input
                   id="firstName"
                   name="firstName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={false}>
+              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
                 <FormLabel htmlFor="email">Email Address</FormLabel>
                 <Input
                   id="email"
                   name="email"
                   type="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
               </FormControl>
               <FormControl>
                 <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
+                <Select
+                  id="type"
+                  name="type"
+                  onChange={formik.handleChange}
+                  value={formik.values.type}
+                >
+                  <option className="option" style={{ backgroundColor: "#512DA8" }} value="hireMe">
+                    Freelance project proposal
+                  </option>
+                  <option className="option" style={{ backgroundColor: "#512DA8" }} value="openSource">
                     Open source consultancy session
                   </option>
-                  <option value="other">Other</option>
+                  <option className="option" style={{ backgroundColor: "#512DA8" }} value="other">
+                    Other
+                  </option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={false}>
+              <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
                 <FormLabel htmlFor="comment">Your message</FormLabel>
                 <Textarea
                   id="comment"
                   name="comment"
                   height={250}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.comment}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
+              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
                 Submit
               </Button>
             </VStack>
